52c3da9096db7f1e5f749a42a16bcab99f5fd061
Hector Santana <hesantan@gmail.com>
2017-04-21 00:48:56 -0400

    Added main solution and projects

---
+++ WindowsPlayground.Test/Properties/AssemblyInfo.cs
@@ -0,0 +1,35 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("WindowsPlayground.Test")]
+[assembly: AssemblyDescription("Test classes for .NET code")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Santana Creations, LLC.")]
+[assembly: AssemblyProduct("WindowsPlayground.Test")]
+[assembly: AssemblyCopyright("Copyright © 2017 Santana Creations, LLC.")]
+[assembly: AssemblyTrademark("Santana Creations, LLC.")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("413d9dca-32ea-449c-8d2d-ae823a74f3c2")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
